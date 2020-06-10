a321eb2cee1781a35ec8e0feaa593cf57b48d590
Ashish D'Souza <sudouser512@gmail.com>
2020-06-10 16:15:14 -0400

    Added dynamic firewatch wallpaper functionality, according to sunrise and sunset

---
+++ opt/bin/firewatch_wallpaper.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# firewatch_wallpaper.py
+
+import os
+from subprocess import Popen, PIPE
+from datetime import datetime, timedelta
+
+
+firewatch_path = '/home/ashish/.firewatch'
+firewatch = 'cliff'
+lat = ''
+lon = ''
+
+sunrise, sunset = Popen(['/usr/bin/sunwait', 'list', lat, lon], stdout=PIPE, stderr=PIPE).communicate()[0].decode().strip().split(', ')
+time = datetime.now()
+sunrise = datetime.strptime(datetime.strftime(time, '%Y%m%d') + '_' + sunrise, '%Y%m%d_%H:%M')
+sunset = datetime.strptime(datetime.strftime(time, '%Y%m%d') + '_' + sunset, '%Y%m%d_%H:%M')
+
+if time < sunrise:
+    # Night
+    if time >= sunrise - timedelta(hours=1, minutes=0):
+        # Before sunrise wallpaper
+        Popen(['/usr/bin/nitrogen', '--save', '--set-zoom-fill', os.path.join(firewatch_path, firewatch, firewatch + '-before-sunrise.jpg')], stdout=PIPE, stderr=PIPE)
+    else:
+        # Night wallpaper
+        Popen(['/usr/bin/nitrogen', '--save', '--set-zoom-fill', os.path.join(firewatch_path, firewatch, firewatch + '-night.jpg')], stdout=PIPE, stderr=PIPE)
+elif time > sunset:
+    # Night
+    if time <= sunset + timedelta(hours=0, minutes=30):
+        # After sunset wallpaper
+        Popen(['/usr/bin/nitrogen', '--save', '--set-zoom-fill', os.path.join(firewatch_path, firewatch, firewatch + '-after-sunset.jpg')], stdout=PIPE, stderr=PIPE)
+    else:
+        # Night wallpaper
+        Popen(['/usr/bin/nitrogen', '--save', '--set-zoom-fill', os.path.join(firewatch_path, firewatch, firewatch + '-night.jpg')], stdout=PIPE, stderr=PIPE)
+else:
+    # Day
+    if time <= sunrise + timedelta(hours=1, minutes=0):
+        # Sunrise wallpaper
+        Popen(['/usr/bin/nitrogen', '--save', '--set-zoom-fill', os.path.join(firewatch_path, firewatch, firewatch + '-sunrise.jpg')], stdout=PIPE, stderr=PIPE)
+    elif time >= sunset - timedelta(hours=1, minutes=0):
+        # Sunset wallpaper
+        Popen(['/usr/bin/nitrogen', '--save', '--set-zoom-fill', os.path.join(firewatch_path, firewatch, firewatch + '-sunset.jpg')], stdout=PIPE, stderr=PIPE)
+    else:
+        # Day wallpaper
+        Popen(['/usr/bin/nitrogen', '--save', '--set-zoom-fill', os.path.join(firewatch_path, firewatch, firewatch + '-day.jpg')], stdout=PIPE, stderr=PIPE)
